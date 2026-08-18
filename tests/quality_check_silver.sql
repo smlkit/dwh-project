3f8d69f7a04976c9f10a0b5303b7afd6debb0b51
ALENAI <79578726+smlkit@users.noreply.github.com>
2026-08-18 16:18:25 +0300

    create quality_check_silver.sql

---
+++ tests/quality_check_silver.sql
@@ -0,0 +1,25 @@
+-- Check for NULLS or Duplicates in PK
+-- Expectation: no results
+SELECT
+	cst_id,
+	COUNT(*) AS PK_count
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+-- Check for unwanted spaces in string columns
+-- Expectation: no Results
+SELECT 
+    cst_firstname 
+FROM silver.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname);
+
+-- Data Standardization & Consistency
+-- Check unique genders & marital status options
+SELECT DISTINCT 
+    cst_gndr
+FROM silver.crm_cust_info;
+
+SELECT DISTINCT 
+    cst_marital_status  
+FROM silver.crm_cust_info;
